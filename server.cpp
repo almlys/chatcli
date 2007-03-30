e5c4c0075bf099ca2653b81669e24393c9f2640d
alberto <alberto@adda2956-d21e-0410-a048-e7034be613bc>
2007-03-30 13:15:15 +0000

    ash
    git-svn-id: http://172.26.0.2/svn/als/mpl/xarxes2/p1@192 adda2956-d21e-0410-a048-e7034be613bc

--- server.cpp
+++ server.cpp
@@ -1,4 +1,36 @@
-
+/******************************************************************************
+* $Revision$                                                            *
+*                                                                             *
+*    Copyright (C) 2007 Ignasi Barri Vilardell                                *
+*    Copyright (C) 2007 Alberto Montañola Lacort                              *
+*    Copyright (C) 2007 Josep Rius Torrento                                   *
+*    See the file AUTHORS for more info                                       *
+*                                                                             *
+*    This program is free software; you can redistribute it and/or modify     *
+*    it under the terms of the GNU General Public License as published by     *
+*    the Free Software Foundation; either version 2 of the License, or        *
+*    (at your option) any later version.                                      *
+*                                                                             *
+*    This program is distributed in the hope that it will be useful,          *
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+*    GNU General Public License for more details.                             *
+*                                                                             *
+*    You should have received a copy of the GNU General Public License        *
+*    along with this program; if not, write to the Free Software              *
+*    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA            *
+*    02110-1301,USA.                                                          *
+*                                                                             *
+*    Please see the file COPYING for the full license.                        *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+* Xarxes II                                                                   *
+* Màster en Enginyeria de Programari Lliure                                   *
+*                                                                             *
+* Pràctica I                                                                  *
+*                                                                             *
+******************************************************************************/
 #include <iostream>
 #include <string>
 #include <exception>
